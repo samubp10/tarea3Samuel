3244bbfc21411146eab22f62fdd743407d0a9dd0
JCarlosVico <jfervic933@maralboran.es>
2019-11-25 20:08:21 +0100

    Añade clase Panel y atributos principales

---
+++ src/main/java/daw/jcarlosvico/Panel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.jcarlosvico;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author jfervic933
+ */
+public class Panel extends JPanel{
+    
+    // Atributos del panel
+    private JButton btnGenerar, btnCerrar;
+    private JLabel lblUrl, lblFichero;
+    private JTextField tfUrl, tfFichero;
+    private String nombreFichero;
+    
+}
