11f5aa6e4a51182fb88dd86ffb69ed827c119d47
JCarlosVico <jfervic933@maralboran.es>
2019-11-25 18:33:08 +0100

    Crea las clases base

---
+++ src/main/java/daw/jcarlosvico/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.jcarlosvico;
+
+import static javax.swing.WindowConstants.*;
+
+
+
+
+/**
+ *
+ * @author jfervic933
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+    }
+}
