da4758473649f34c0595d462b587d8078fc6d0f7
JCarlosVico <jfervic933@maralboran.es>
2019-11-27 17:53:02 +0100

    Añade clase QR

---
+++ src/main/java/daw/jcarlosvico/QR.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.jcarlosvico;
+
+/**
+ *
+ * @author carlos
+ */
+public class QR {
+    
+    // Constantes. Tamaño del QR
+    public static final int ANCHO_QR = 400;
+    public static final int ALTO_QR = 400;
+    
+    // Método que genera el archivo de imagen
+    // a partir del texto almacenado en la variable "texto"
+    public static void escribirQR(String texto, String fichero){
+        
+    }
+    
+}
