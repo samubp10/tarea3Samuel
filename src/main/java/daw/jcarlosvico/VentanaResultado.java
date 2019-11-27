e962ec7a1ac1f9ea4e807b499433557f07e45903
JCarlosVico <jfervic933@maralboran.es>
2019-11-27 19:24:23 +0100

    Ajustes finales para centrar la ventana resultado

--- src/main/java/daw/jcarlosvico/VentanaResultado.java
+++ src/main/java/daw/jcarlosvico/VentanaResultado.java
@@ -5,6 +5,8 @@
  */
 package daw.jcarlosvico;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,16 +22,26 @@ public class VentanaResultado extends JFrame {
     // Este constructor recibe el fichero que contiene 
     // el QR, lo establece en el JLabel y se muestra
     public VentanaResultado(String fichero) {
-        // Se crea un objeto imagen a partir del fichero del QR
-        ImageIcon icono = new ImageIcon(fichero);
         // Título del frame
         this.setTitle(fichero);
+        // No se redimensiona
+        this.setResizable(false);
+        // Objeto dimension para guardar información del tamaño de la pantalla
+        Dimension tamanioPantalla = Toolkit.getDefaultToolkit().getScreenSize();
+        // Posición inicial de la ventana. Centrada respecto a la pantalla
+        this.setBounds(tamanioPantalla.width/2 - QR.ANCHO_QR/2, 
+                    tamanioPantalla.height/2 - QR.ALTO_QR/2, 
+                QR.ANCHO_QR, QR.ALTO_QR);
         // Inicialización del atributo
         resultado = new JLabel();
+        // Se crea un objeto imagen a partir del fichero del QR
+        ImageIcon icono = new ImageIcon(fichero);
         // Se establece la imagen del QR en el JLabel
         resultado.setIcon(icono);
         this.add(resultado);
+        // Ajusta el tamaño de la ventana al contenido
         this.pack();
+        // Hace visible la ventana
         this.setVisible(true);
     }
 }
