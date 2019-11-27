2bff8f61841b0ccf00a35f4198b535d192549fc3
JCarlosVico <jfervic933@maralboran.es>
2019-11-27 18:47:05 +0100

    Elimina imports sin uso

--- src/main/java/daw/jcarlosvico/QR.java
+++ src/main/java/daw/jcarlosvico/QR.java
@@ -14,8 +14,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
